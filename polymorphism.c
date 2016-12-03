5d2cdc92a9259af489a846c5b781fff3b4878a13
James Hui <james.hui@uk.thalesgroup.com>
2016-12-03 11:34:11 +0000

    virtual table added

---
+++ polymorphism.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <assert.h>
+
+struct ShapeVtbl;
+typedef struct {
+  struct ShapeVtbl const *vptr;
+	int x;
+	int y;
+} Shape;
+
+struct ShapeVtbl {
+  int (*area)(Shape* const me);
+	void (*draw)(Shape* const me);
+};
+
+static int Shape_area_ (Shape* const me) {
+  assert(0);
+	return 0;
+};
+static void Shape_draw_ (Shape* const me){
+  assert(0);
+};
+
+void Shape_Constructor(Shape* const me, int x, int y) {
+  static struct ShapeVtbl const vtbl = {
+	  &Shape_area_,
+		&Shape_draw_
+	};
+	me->vptr = &vtbl;
+	
+	me->x;
+	me->y;
+}
+
+int main(void) {
+  return 0;
+}
